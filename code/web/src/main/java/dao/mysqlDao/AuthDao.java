673f26811c12bcc4d9b285ba3dfba35023b00f03
rudeigerc <rudeigerc@gmail.com>
2017-06-27 17:48:48 +0800

    merge

---
+++ code/web/src/main/java/dao/mysqlDao/AuthDao.java
@@ -0,0 +1,7 @@
+package dao.mysqlDao;
+
+/**
+ * Created by qjr on 2017/6/27.
+ */
+public interface AuthDao {
+}
---
+++ code/web/src/main/java/model/mysql/Notebook.java
@@ -0,0 +1,7 @@
+package model.mysql;
+
+/**
+ * Created by qjr on 2017/6/27.
+ */
+public class Notebook {
+}
