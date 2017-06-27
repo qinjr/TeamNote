b2e30320fc8641a1e55863278253f2b8e8f9684f
qjr <qinjr@icloud.com>
2017-06-28 00:38:37 +0800

    fix bad design

--- code/web/src/main/java/service/CooperateService.java
+++ code/web/src/main/java/service/CooperateService.java
@@ -8,20 +8,22 @@ import java.util.Date;
 public interface CooperateService {
     /**
      * inviteCooperator
-     * @param userId 邀请者Id
+     * @param inviterId 邀请者Id
+     * @param targetId 受邀者Id
      * @param notebookId 笔记本Id
      * @return 1为请求发送成功，0为失败
      */
-    public int inviteCooperator(int userId, int notebookId);
+    public int inviteCooperator(int inviterId, int targetId, int notebookId);
 
 
     /**
      * takeInvitation
      * @param userId 受邀者Id
      * @param notebookId 笔记本Id
+     * @param decision 笔记本Id
      * @return 1为接受，0为拒绝
      */
-    public int takeInvitation(int userId, int notebookId);
+    public int checkInvitation(int userId, int notebookId, int decision);
 
 
     /**
@@ -50,17 +52,19 @@ public interface CooperateService {
      * @param noteId 提出意见的noteId
      * @param content 修改后的笔记内容
      * @param description 解释修改的原因
-     * @return 审核contributor修改的链接
+     * @param datetime 提交修改建议的时间
+     * @return 1为提交成功，0为提交失败
      */
-    public String raiseAdvice(int userId, int noteId, String content, String description);
+    public int raiseAdvice(int userId, int noteId, String content, String description, Date datetime);
 
 
     /**
      *
-     * @param userId 提出意见者的Id
+     * @param advicerId 提出意见者的Id
+     * @param managerId 笔记管理者的Id
      * @param noteId 被提出意见的note的Id
      * @param datetime 提出意见的时间（在Suggestion表中寻找到要merge的版本）（做为审核contributor修改的链接的一部分传入）
      * @return 1为接受意见merge成功，0为未merge
      */
-    public int mergeAdvice(int userId, int noteId, Date datetime);
+    public int mergeAdvice(int advicerId, int managerId, int noteId, Date datetime);
 }
