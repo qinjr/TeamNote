4c8a8cc38e5b3d806a0ea0c9b542dbbd48e66ee8
qjr <qinjr@icloud.com>
2017-06-30 16:28:50 +0800

    add a few dependencies
    mysql dao impls

---
+++ code/web/src/main/java/dao/mysqlDaoImpl/AuthDaoImpl.java
@@ -0,0 +1,35 @@
+package dao.mysqlDaoImpl;
+
+import dao.mysqlDao.AuthDao;
+import model.mysql.Auth;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by qjr on 2017/6/30.
+ */
+public class AuthDaoImpl extends HibernateDaoSupport implements AuthDao {
+    public Integer addAuth(Auth auth) {
+        return (Integer) getHibernateTemplate().save(auth);
+    }
+
+    public void deleteAuth(Auth auth) {
+        getHibernateTemplate().delete(auth);
+    }
+
+    public void updateAuth(Auth auth) {
+        getHibernateTemplate().merge(auth);
+    }
+
+    public Auth getAuthById(int authId) {
+        List<Auth> auths = (List<Auth>) getHibernateTemplate().find("from Auth as a where a.authId=?", authId);
+        return auths.size() > 0 ? auths.get(0) : null;
+    }
+
+    public ArrayList<Auth> getAllAuths() {
+        ArrayList<Auth> auths = (ArrayList<Auth>) getHibernateTemplate().find("from Auth");
+        return auths;
+    }
+}
