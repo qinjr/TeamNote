e1233fece3f9bc47aef8b25b10cf4906e9772f12
rudeigerc <rudeigerc@gmail.com>
2017-07-04 18:47:53 +0800

    add notebook tag

--- code/web/web/js/cooperate.js
+++ code/web/web/js/cooperate.js
@@ -28,8 +28,44 @@ $(document).ready(function() {
             url: "cooperate/allnotebooks",
             type: "get",
             success: function(data) {
+                document.getElementById("workgroup").innerHTML = "";
                 var json = JSON.parse(data);
-                var el = document.getElementById("workgroup")
+                for (var notebook in json) {
+                    var html = '<div class="row">'+
+                        '<div class="col-md-12" style="margin-top: 20px;">' +
+                        '<div class="row">' +
+                        '<div class="col-md-2 text-center mx-auto">' +
+                        '<img id="cover" src="'+ "image/" + json[notebook].cover +'" style="height: 75px; width: 75px;">' +
+                        '</div>' +
+                        '<div class="col-md-7">' +
+                        '<h4 class="card-title">' + json[notebook].title + '</h4>' +
+                        '<h6 class="card-subtitle mb-2 text-muted"> ' +
+                        // TODO: tag?
+                        '<i class="fa fa-tag" aria-hidden="true"></i>&nbsp;机器学习 · Logistic 回归' +
+                        '</h6>' +
+                        // TODO: userID => user
+                        // TODO: date format
+                        '<small>创建者 <strong>rudeigerc</strong> · 所有者 <strong>rudeigerc</strong> · 修改时间 2017-06-03 23:34:23</small>' +
+                        '<br>' +
+                        '<div style="margin: 10px auto;">' +
+                        '<img src="image/test.jpg" style="width: 50px; height: 50px;">&nbsp;' +
+                        '<img src="image/guest.png" style="width: 50px; height: 50px;">' +
+                        '</div>' +
+                        '</div>' +
+                        '<div class="col-md-3 workgroup-btn">' +
+                        '<a class="btn btn-outline-primary center-block" role="button" href="workgroup.jsp">' +
+                        '<i class="fa fa-users fa-fw" aria-hidden="true"></i>&nbsp;进入工作组' +
+                        '</a>' +
+                        '<button class="btn btn-outline-warning center-block" type="button">' +
+                        '<i class="fa fa-envelope-open fa-fw" aria-hidden="true"></i>&nbsp;邀请用户' +
+                        '</button>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '<div class="dropdown-divider"></div>';
+                    $('#workgroup').append(html);
+                }
             }
         });
 
