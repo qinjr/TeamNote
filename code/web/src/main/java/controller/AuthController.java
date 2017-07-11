eec82e0a8450e8014abbaf2d871860f5150c74a5
qjr <qinjr@icloud.com>
2017-07-11 14:41:05 +0800

    register have default avator

--- code/web/src/main/java/controller/AuthController.java
+++ code/web/src/main/java/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
     @RequestMapping("/register")
     public String register(@RequestParam(value = "_username") String username, @RequestParam(value = "_password") String password,
                            @RequestParam(value = "phone") String phone, @RequestParam(value = "email") String email) {
-        if (userBasicService.register(username, password, phone, email, "", null, new ArrayList<Integer>()) == 1) {
+        if (userBasicService.register(username, password, phone, email, "", "image/avator/default_avator.png", new ArrayList<Integer>()) == 1) {
             return "auth";
         } else {
             return "auth";
