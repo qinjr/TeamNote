c88f18a39f4637700ad2c7f706e0e6cd0810cd1a
qjr <qinjr@icloud.com>
2017-07-13 11:06:59 +0800

    chat test

--- code/web/src/main/java/controller/ViewController.java
+++ code/web/src/main/java/controller/ViewController.java
@@ -4,11 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by rudeigerc on 2017/7/12.
+ * Created by qjr on 2017/7/13.
  */
-
 @Controller
-public class HomepageController {
+public class ViewController {
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @RequestMapping(path = {"/index", "/"})
+    public String index() {
+        return "index";
+    }
 
     @RequestMapping("/homepage")
     public String homepage() {
